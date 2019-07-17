f308232020ae23683e34fd27b6692c05a793711e
Yvonne <giryvonne@gmail.com>
2019-07-17 16:28:27 +0200

    add paragraph

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var maleNames=myFunction["Kwadwo" "Kwabena" "Kwaku" " Yaw" "Kofi" "Kwame" "Kwasi"];
+
+function myFunction("Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday" "Sunday")
+{
+return(maleNames);
+}
+document.getElementById("demo").innerHTML = maleNames;
