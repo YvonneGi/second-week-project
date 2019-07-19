26719bf8d82a6aba413170945201708be4f7876a
Yvonne <giryvonne@gmail.com>
2019-07-19 12:32:14 +0200

    make changes on the form content

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
-var YY=parseInt(prompt("Enter year of birth"));
-var MM=parseInt(prompt("Enter month of birth"));
-var DD=parseInt(prompt("Enter your day of birth"));
-var CC=parseInt(prompt("Enter century"));
-var gender=prompt("enter your gender");
-var dayOfTheWeek=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
-dayOfTheWeek.toFixed(0);
-var day=[0,1,2,3,4,5,6];
-if(day==0&&gender==male)
+var YY = document.getElementById("years").value;
+var MM = document.getElementById("months").value;
+var DD = document.getElementById("days").value;
+//var CC = document.getElementById("date").value;
+var gender = document.getElementById("gender").value;
+var male = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame];
+var female = [Akosua, Adwoa, Abenaa, Akua, Yaa, Afua,Ama];
+var day = new Date();
+day = day.getDay();
+
+// var dayOfTheWeek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
+// dayOfTheWeek.toFixed(0);
+/*var day = [0,1,2,3,4,5,6];
+day = Date();
+*/
+
+
+if(day == 0 && gender == male)
 {
     alert("Kwasi");
 }
-else if(day==1&&gender==male)
+else if(day == 1 && gender == male)
 {
     alert("Kwadwo");
 
 }
-else if(day==2&&gender==male)
+else if(day == 2 && gender == male)
 {
     alert("Kwabena");
 
 }
-else if(day==3&&gender==male){
+else if(day == 3 && gender == male){
     alert("Kwaku");
 }
-else if(day==4&&gender==male){
+else if(day== 4 && gender == male){
     alert("Yaw");
 }
-else if(day==5&&gender==male){
-    alert("Kofi");
+else if(day == 5 && gender == male){
+    alert("Kofii");
 }
-else if(day==6&&gender==male){
+else if(day == 6 && gender == male){
     alert("Kwame");
 }
-
+else{
+    alert("enter correct date of birth and gender");
+}
+function dayOfTheWeek(){
+    for(i = 0; i >= male.length )
+    return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
+}
