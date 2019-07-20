f38144c20d44a84fb49d83d14d61244beac8337b
Yvonne <giryvonne@gmail.com>
2019-07-20 16:24:31 +0200

    add changes in html

--- javas/script.js
+++ javas/script.js
@@ -7,65 +7,65 @@ var MM = document.getElementById("months").value;
 console.log(MM);
 var DD = document.getElementById("days").value;
 console.log(DD);
-var gender = document.getElementById("m").value;
-console.log(gender);
-var gender = document.getElementById ("f").value;
-console.log(gender);
+var genderm = document.getElementById("m").value;
+console.log(genderm);
+var genderf = document.getElementById ("f").value;
+console.log(genderf);
     var day =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
     var answer = Math.floor(day);
 
-          if(answer === 0 && gender === "Male"){
+          if(answer === 0 && genderm === "Male"){
            document.getElementById("demo").innerHTML = "Your Akan name is Kwasi";
          
       }
-            else if(answer === 1 && gender === "Male")
+            else if(answer === 1 && genderm === "Male")
        {
     document.getElementById("demo").innerHTML = "Your Akan name is Kwadwo";
    
    }
-      else if(answer === 2 && gender === "Male")
+      else if(answer === 2 && genderm === "Male")
 {
     document.getElementById("demo").innerHTML = "Your Akan name is Kwabena";
    
 }
-else if(answer === 3 && gender === "Male"){
+else if(answer === 3 && genderm === "Male"){
    document.getElementById("demo").innerHTML = "Your Akan name is Kwaku";
   
 }
-else if(answer === 4 && gender === "Male"){
+else if(answer === 4 && genderm === "Male"){
     document.getElementById("demo").innerHTML = "Your Akan name is Yaw";
    
 }
-else if(answer === 5 && gender === "Male"){
+else if(answer === 5 && genderm === "Male"){
     document.getElementById("demo").innerHTML = "Your Akan name is Kofii";
     
 }
-else if(answer === 6 && gender === "Male"){
+else if(answer === 6 && genderm === "Male"){
     document.getElementById("demo").innerHTML = "Your Akan name is Kwame";
     
 }  
 
 
-        else if(answer === 0 && gender === "Female"){
+        else if(answer === 0 && genderf === "Female"){
            document.getElementById("demo").innerHTML = "Your Akan name is Akosua" ;
            
         }
-        else if(answer === 1 && gender === "Female") {
+        else if(answer === 1 && genderf === "Female") {
             document.getElementById("demo").innerHTML = "Your Akan name is Adwoa"; 
         }
-        else if(answer === 2 && gender === "Female"){
+        else if(answer === 2 && genderf === "Female"){
             document.getElementById("demo").innerHTML = "Your Akan name is Abenaa";  
         }
-        else if(answer === 3 && gender === "Female"){
+        else if(answer === 3 && genderf === "Female"){
             document.getElementById("demo").innerHTML = "Your Akan name is Akua"; 
         }
-        else if(answer === 4 && gender === "Female"){
+        else if(answer === 4 && genderf === "Female"){
             document.getElementById("demo").innerHTML = "Your Akan name is Yaa"; 
         }
-        else if(answer === 5 && gender === "Female"){
+        else if(answer === 5 && genderf === "Female"){
             document.getElementById("demo").innerHTML = "Your akan name is Afua"; 
         }
-        else if(answer === 6 && gender === "Female"){
+        else if(answer === 6 && genderf === "Female"){
             document.getElementById("demo").innerHTML = "Your Akan name is Ama"; 
         }
            
