87d5a69bfad18d17fe14706984a09617aaa21a97
Yvonne <giryvonne@gmail.com>
2019-07-23 13:21:26 +0200

    make indentation of my script file

--- javas/script.js
+++ javas/script.js
@@ -10,31 +10,7 @@ var genderm = document.getElementById("m").value;
 var genderf = document.getElementById ("f").value;
 var day =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
 var answer = Math.floor(day);
-if(answer === 0 && genderf === "Female")
-{
-    document.getElementById("demo").innerHTML = "Your Akan name is Akosua" ;
-}
-    else if(answer === 1 && genderf === "Female") {
-    document.getElementById("demo").innerHTML = "Your Akan name is Adwoa"; 
-}
-    else if(answer === 2 && genderf === "Female"){
-    document.getElementById("demo").innerHTML = "Your Akan name is Abenaa";  
-}
-    else if(answer === 3 && genderf === "Female"){
-    document.getElementById("demo").innerHTML = "Your Akan name is Akua"; 
-}
-    else if(answer === 4 && genderf === "Female"){
-    document.getElementById("demo").innerHTML = "Your Akan name is Yaa"; 
-}
-    else if(answer === 5 && genderf === "Female"){
-    document.getElementById("demo").innerHTML = "Your akan name is Afua"; 
-}
-    else if(answer === 6 && genderf === "Female"){
-    document.getElementById("demo").innerHTML = "Your Akan name is Ama"; 
-}
-   
-
-    else if(answer === 0 && genderm === "Male")
+if(answer === 0 && genderm === "Male")
 {
     document.getElementById("demo").innerHTML = "Your Akan name is Kwasi";
          
@@ -70,8 +46,29 @@ if(answer === 0 && genderf === "Female")
 {
     document.getElementById("demo").innerHTML = "Your Akan name is Kwame";
     
-}     
-        
+} 
+    else if(answer === 0 && genderf === "Female")
+{
+    document.getElementById("demo").innerHTML = "Your Akan name is Akosua" ;
+}
+    else if(answer === 1 && genderf === "Female") {
+    document.getElementById("demo").innerHTML = "Your Akan name is Adwoa"; 
+}
+    else if(answer === 2 && genderf === "Female"){
+    document.getElementById("demo").innerHTML = "Your Akan name is Abenaa";  
+}
+    else if(answer === 3 && genderf === "Female"){
+    document.getElementById("demo").innerHTML = "Your Akan name is Akua"; 
+}
+    else if(answer === 4 && genderf === "Female"){
+    document.getElementById("demo").innerHTML = "Your Akan name is Yaa"; 
+}
+    else if(answer === 5 && genderf === "Female"){
+    document.getElementById("demo").innerHTML = "Your akan name is Afua"; 
+}
+    else if(answer === 6 && genderf === "Female"){
+    document.getElementById("demo").innerHTML = "Your Akan name is Ama"; 
+}    
     else
 {
     document.getElementById("demo").innerHTML ="Please enter correct date of birth and gender";
